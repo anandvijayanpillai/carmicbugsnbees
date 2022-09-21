93b7cb69535aad9eeae371c7501bdb3140924421
anandvijayanpillai <anandvijay136@gmail.com>
2022-09-22 02:48:05 +0530

    news project

--- config/database.js
+++ config/database.js
@@ -6,6 +6,6 @@ module.exports = ({ env }) => ({
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true,git 
   },
 });
